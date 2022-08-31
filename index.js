7db5b35ad50e0eb3047be6147d54f05d35d291d0
Kevin2211 <trinh_thanh42@yahoo.com>
2022-08-31 14:41:43 -0500

    part 2 completed

--- index.js
+++ index.js
@@ -12,13 +12,28 @@ function renderMovies(movieArray){
               <div class="year">${currentMovie.Year}</div>
               <h5 class="card-title">${currentMovie.Title}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Add</a>
+              <a data-imdbid=${currentMovie.imdbID} class="btn btn-primary add-button">Add</a>
             </div>
           </div>
           </div>`
     })
         
-    return movieHTMLArr.join('');
+    return movieHTMLArr.join('')
+}
+
+function saveToWatchList(ID){
+    const movie = movieData.find((currentMovie) =>{
+        return currentMovie.imdbID == ID;
+    })
+    let watchlistJSON = localStorage.getItem('watchlist');
+    let watchlist = JSON.parse(watchlistJSON);
+    if(watchlist == null){
+        watchlist = [];
+    }
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+    console.log(watchlist)
 }
 
 
@@ -27,4 +42,11 @@ myForm.addEventListener('submit', (e) => {
     e.preventDefault();
     movieContainer.innerHTML = renderMovies(movieData);
 })
+
+document.addEventListener('click', function(e) {
+    if(e.target.classList.contains('add-button')){
+        const movieID = e.target.dataset.imdbid;
+        saveToWatchList(movieID);
+    }
+    })
 
