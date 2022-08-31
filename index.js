88b2a79fb6f0568304a3f0efba72cb53a1b42f64
Kevin2211 <trinh_thanh42@yahoo.com>
2022-08-31 09:46:55 -0500

    added render movie function

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const card = document.querySelector('.movie');
+const movieContainer = document.querySelector('.movies-container');
+
+function renderMovies(movieArray){
+    const movieHTMLArr = movieArray.map((currentMovie) =>{
+        return `<div class="col movie">
+          <div class="card " style="width: 18rem;">
+            <img src="${currentMovie.Poster}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <div class="year">${currentMovie.Year}</div>
+              <h5 class="card-title">${currentMovie.Title}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Add</a>
+            </div>
+          </div>
+          </div>`
+    })
+        
+    return movieHTMLArr.join('');
+}
+
+movieContainer.innerHTML = renderMovies(movieData);
+
+
